c15849fd3ac557a5d57bdccc911e3026cbc4a2e1
orma5 <po.markstrom@gmail.com>
2019-10-09 20:54:01 +0200

    Added fetch and store for humidity and pressure Added UOM for each stored value

--- statecapture/sensorTest.py
+++ statecapture/sensorTest.py
@@ -18,16 +18,28 @@ api_url = api_host+api_key+api_sub_section
 resp = requests.get(api_url)
 
 resp_dict = resp.json()
-temperature = resp_dict["2"]["state"]["temperature"]
-lastupdated = resp_dict["2"]["state"]["lastupdated"]
+temperature = resp_dict["2"]["state"]["temperature"] / 100.0
+lastupdated_2 = resp_dict["2"]["state"]["lastupdated"]
+humidity = resp_dict["3"]["state"]["humidity"] / 100.0
+lastupdated_3 = resp_dict["3"]["state"]["lastupdated"]
+pressure = resp_dict["4"]["state"]["pressure"]
+lastupdated_4 = resp_dict["4"]["state"]["lastupdated"]
 
 #Connect to database and write latest sensor state
 mydb = mysql.connector.connect(host=host,user=user,passwd=passwd)
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO homeauto.state_data(lastupdated,state_type,state_value) values(%s,'temperature',%s)"
-val = (lastupdated,temperature)
+sql = "INSERT INTO homeauto.state_data(lastupdated,state_type,state_value,state_uom) values(%s,'temperature',%s,'C')"
+val = (lastupdated_2,temperature)
+mycursor.execute(sql,val)
+mydb.commit()
+sql = "INSERT INTO homeauto.state_data(lastupdated,state_type,state_value,state_uom) values(%s,'humidity',%s,'pp')"
+val = (lastupdated_3,humidity)
+mycursor.execute(sql,val)
+mydb.commit()
+sql = "INSERT INTO homeauto.state_data(lastupdated,state_type,state_value,state_uom) values(%s,'pressure',%s,'hPa')"
+val = (lastupdated_3,pressure)
 mycursor.execute(sql,val)
 mydb.commit()
 mydb.close()
